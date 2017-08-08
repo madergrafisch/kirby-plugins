0c97f255adf6fbb5777deb2e7426acce92871458
Christian Zehetner <mail@christianzehetner.at>
2017-08-08 08:38:26 +0200

    Cleanup.

---
+++ src/Toolkit/html5.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace mgf\Toolkit;
+
+class Html5 extends Html {
+
+  static public function time($timestamp, $format = 'short', $classes = '') {
+    $brick = new Brick('time');
+    $brick->datetime = Date::format($timestamp, 'iso');
+    $brick->text(Date::format($timestamp, $format));
+    $brick->addClass($classes);
+    return $brick;
+  }
+
+}
--- xmlsitemap/xmlsitemap.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-function xmlsitemap () {
-  return tpl::load(__DIR__ . DS . 'template.php', array('pages' => kirby()->site()->pages()));
-}
